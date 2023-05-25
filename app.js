0fc9cd18378fd1b1ce82cfad2ced268aa2770c51
SofiaRzz <sofiarizzottobrasil@gmail.com>
2023-05-25 14:00:33 -0300

    finished alunos

--- app.js
+++ app.js
@@ -6,32 +6,86 @@ const port = 3000;
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
-app.get("/", (req, res) => {
-    res.send("Hello World")
-})
-
-app.listen(port, () => {
-    console.log("Servidor rodando")
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const alunos = [
-    {nome: "Sofia",
+  {
+    id: 1,
+    nome: "Sofia",
     dataNasc: "2006-01-07",
     matricula: "2021321147",
-    status: true,
-    email: "spr2@aluno.ifal.edu.br"
-    },
-    {nome: "Lucas",
+    status: "Ativa",
+    email: "spr2@aluno.ifal.edu.br",
+  },
+  {
+    id: 2,
+    nome: "Lucas",
     dataNasc: "2005-04-16",
     matricula: "2021319327",
-    status: true,
-    email: "mlfd1@aluno.ifal.edu.br"
-    },
-    {nome: "Fernanda",
+    status: "Ativa",
+    email: "mlfd1@aluno.ifal.edu.br",
+  },
+  {
+    id: 3,
+    nome: "Fernanda",
     dataNasc: "2006-05-12",
     matricula: "2021312245",
-    status: true,
-    email: "fam2@aluno.ifal.edu.br"
-    }
+    status: "Ativa",
+    email: "fam2@aluno.ifal.edu.br",
+  },
+];
 
-]
+app.get("/", (req, res) => {
+  res.render("home", { alunos: alunos });
+});
+
+app.get("/cad-alt/:id?", (req, res) => {
+  if (req.params.id) {
+    const id = Number(req.params.id);
+    const aluno = alunos.find((aluno) => aluno.id == id);
+    res.render("cad-alt", { aluno: aluno });
+  } else {
+    res.render("cad-alt", { aluno: null });
+  }
+  res.render("cad-alt");
+});
+
+app.delete("/alunos/:id", (req, res) => {
+  const id = Number(req.params.id);
+  for (let i = 0; i < alunos.length; i++) {
+    if (alunos[i].id == id) {
+      alunos.splice(i, 1);
+      break;
+    }
+  }
+});
+
+app.post("/alunos/:id?", (req, res) => {
+  if (req.params.id) {
+    const id = Number(req.params.id);
+    const index = alunos.findIndex((aluno) => aluno.id == id);
+    alunos[index] = {
+      id: id,
+      nome: req.body.nome,
+      dataNasc: req.body.dataNasc,
+      matricula: req.body.matricula,
+      status: req.body.status,
+      email: req.body.email,
+    };
+  } else {
+    alunos.push({
+      id: alunos.at(-1).id + 1,
+      nome: req.body.nome,
+      dataNasc: req.body.dataNasc,
+      matricula: req.body.matricula,
+      status: req.body.status,
+      email: req.body.email,
+    });
+  }
+  res.redirect("/");
+});
+
+app.listen(port, () => {
+  console.log("Servidor rodando");
+});
